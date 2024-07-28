1523ba190833d859952ef72a2f0830cab98d9bf4
DeePlusPlus <r2dan2d2@gmail.com>
2024-07-27 22:26:56 -0400

    Basic usage in place.  Would like to make it more like a give and take console app.

--- main.py
+++ main.py
@@ -1,5 +1,28 @@
+import os
+import logging
+from openai import OpenAI
+from dotenv import load_dotenv
+
+
 def main(): 
-    print("hey there") 
+    load_dotenv() 
+    client = OpenAI(
+        organization=os.getenv("ORGANIZATION_ID"),
+        api_key=os.getenv("SECRET_KEY"),
+        project=os.getenv("PROJECT_ID")
+    )
+
+    chat_completion = client.chat.completions.create(
+        messages=[
+            {
+                "role": "user",
+                "content": "Say this is a test",
+            }
+        ],
+        model="gpt-3.5-turbo",
+    )
+    print(chat_completion.choices[0].message.content)
+
   
   
 if __name__=="__main__": 
